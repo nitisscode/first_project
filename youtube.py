7b8820bc28f6366f3468b7db6b4ce93edbad6617
nitisscode <140188297+nitisscode@users.noreply.github.com>
2023-07-22 10:10:58 +0530

    Add files via upload

--- youtube.py
+++ youtube.py
@@ -3,16 +3,25 @@ from pytube import YouTube
 link=input("Enter link: ")
 yt=YouTube(link)
 
+print("choose 'audio' or 'video'")
+option=input("Enter choice: ")
+
 print(yt.title)
 print(yt.thumbnail_url)
 
 print("Downloading...")
+def saveaudio():
+    audio=yt.streams
+    audio.get_audio_only().download()
 
-audio=yt.streams
-audio.get_audio_only().download()
+def savevideo():
+    video=yt.streams.get_lowest_resolution()
+    video.download()
 
-video=yt.streams.get_lowest_resolution()
-video.download()
+if(option=="audio"):
+    saveaudio()
+if(option=="video"):
+    savevideo()
 
 print("Download completed")
 
